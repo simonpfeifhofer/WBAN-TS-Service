a874d3c9987ed2d1643816ee44b8c63d300d2756
Simon Pfeifhofer <simon.pfeifhofer@gmail.com>
2014-07-02 20:11:25 +0200

    Reorganize, add Live-Monitor

---
+++ WBANTSLiveMonitor/js/custom/chart.js
@@ -0,0 +1,67 @@
+﻿
+function CustomChart(graphElementId) {
+
+    var _internalChart = null;
+    var _graphElementId = graphElementId;
+
+    this.Draw = function(){
+
+        var dates = ['15:11', '15:12', '15:17', '15:18'];
+        var values = [117, 162, 154, 132];
+
+        var data = {
+            labels: dates,
+            datasets: [
+                {
+                    label: "HRM",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: values
+                }
+            ]
+        };
+
+        var options =
+            {
+                scaleShowGridLines: true,
+                //String - Colour of the grid lines
+                scaleGridLineColor: "rgba(0,0,0,.05)",
+                //Number - Width of the grid lines
+                scaleGridLineWidth: 0,
+                //Boolean - Whether the line is curved between points
+                bezierCurve: true,
+                //Number - Tension of the bezier curve between points
+                bezierCurveTension: 0.4,
+                //Boolean - Whether to show a dot for each point
+                pointDot: true,
+                //Number - Radius of each point dot in pixels
+                pointDotRadius: 4,
+                //Number - Pixel width of point dot stroke
+                pointDotStrokeWidth: 0,
+                //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                pointHitDetectionRadius: 0,
+                //Boolean - Whether to show a stroke for datasets
+                datasetStroke: true,
+                //Number - Pixel width of dataset stroke
+                datasetStrokeWidth: 2,
+                //Boolean - Whether to fill the dataset with a colour
+                datasetFill: true,
+                //String - A legend template
+                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+            };
+
+        var ctx = document.getElementById(_graphElementId).getContext("2d");
+        _internalChart = new Chart(ctx).Line(data, options);
+
+    }
+
+}
+
+$(document).ready(function () {
+    var chart = new CustomChart("chart");
+    chart.Draw();
+});
---
+++ WBANTSLiveMonitor/js/custom/map.js
@@ -0,0 +1,43 @@
+﻿
+function Map(elementId) {
+
+    var _elementId = elementId;
+    var _internalMap = null;
+
+    var initialize = function(){
+        var map_canvas = document.getElementById(_elementId);
+        var map_options = {
+            center: new google.maps.LatLng(44.5403, -78.5463),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        _internalMap = new google.maps.Map(map_canvas, map_options);
+        drawRoute();
+    }
+
+    var drawRoute = function(){
+
+        var routeCoordinates = [
+            new google.maps.LatLng(44.5403, -78.5463),
+            new google.maps.LatLng(21.291982, -157.821856),
+            new google.maps.LatLng(-18.142599, 178.431),
+            new google.maps.LatLng(-27.46758, 153.027892)
+        ];
+        var route = new google.maps.Polyline({
+            path: routeCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        route.setMap(_internalMap);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+}
+
+$(document).ready(function () {
+    var map = new Map("map");
+});
